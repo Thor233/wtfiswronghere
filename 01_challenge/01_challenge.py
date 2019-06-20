d109d89ac7883c5ddbaea8fc7ba3e6dde4cfb697
谭东 <51953379+Thor233@users.noreply.github.com>
2019-06-21 06:59:17 +0800

    Update 01_challenge.py
    增加一些空格以满足书写规范，27行处应当使用等号 而非 赋值

--- 01_challenge/01_challenge.py
+++ 01_challenge/01_challenge.py
@@ -20,15 +20,15 @@ def fizzbuzz(max_num):
     num2 = 5 
 
     # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
+    for i in range(1, max_num):
         # % or modulo division gives you the remainder 
-        if i%num1==0 and i%num2==0:
-            print(i,three_mul+five_mul)
-        elif i%num1=0:
-            print(i,three_mul)
-        elif i%num2==0:
-            print(i,five_mul)
+        if i % num1 == 0 and i % num2 == 0:
+            print(i, three_mul + five_mul)
+        elif i % num1 == 0:
+            print(i, three_mul)
+        elif i % num2 == 0:
+            print(i, five_mul)
 
 #----START OF SCRIPT
-if __name__=='__main__':
+if __name__ == '__main__':
     fizzbuzz(100)
