4bb033694b31189d55e7e8b6ec1db2bb51ca1a73
谭东 <51953379+Thor233@users.noreply.github.com>
2019-06-21 07:08:40 +0800

    Update 02_challenge.py

--- 02_challenge/02_challenge.py
+++ 02_challenge/02_challenge.py
@@ -12,15 +12,16 @@ For all integers between 1 and 99 (include both):
 def fizzbuzz(max_num):
     "This method implements FizzBuzz"
     # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
+    for i in range(1, max_num):
         # % or modulo division gives you the remainder 
-        if i%3==0 and i%5==0:
-            print(i,"fizzbuzz")
-        elif i%3==0:
-            print(i,"fizz")
-        elif i%5==0:
-            print(i,"Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            print(i, "fizzbuzz")
+        elif i % 3 == 0:
+            print(i, "fizz")
+        elif i % 5 == 0:
+            print(i, "Buzz")
 
 #----START OF SCRIPT
-if __name__=='__main__':
-    fizzbuzz()
+if __name__ == '__main__':
+    num = int(input('Enter a int:'))
+    fizzbuzz(num)
