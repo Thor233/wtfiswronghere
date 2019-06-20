be1c42787dbe90666884e185286015078365efd7
谭东 <51953379+Thor233@users.noreply.github.com>
2019-06-21 07:32:38 +0800

    Update 06_challenge.py

--- 06_challenge/06_challenge.py
+++ 06_challenge/06_challenge.py
@@ -18,17 +18,17 @@ def fizzbuzz(max_num):
     three_mul = 'fizz'
     five_mul = 'buzz'
     num1 = conf.num1
-    num2 = conf.num
+    num2 = conf.num2
     # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
+    for i in range(1, max_num):
         # % or modulo division gives you the remainder 
-        if i%num1==0 and i%num2==0:
-            print(i,three_mul+five_mul)
-        elif i%num1==0:
-            print(i,three_mul)
-        elif i%num2==0:
-            print(i,five_mul)
+        if i % num1 == 0 and i % num2 == 0:
+            print(i, three_mul + five_mul)
+        elif i % num1 == 0:
+            print(i, three_mul)
+        elif i % num2 == 0:
+            print(i, five_mul)
 
 #----START OF SCRIPT
-if __name__=='__main__':
+if __name__ == '__main__':
     fizzbuzz(100)
